d27bc87e5ee2d2c5744890528117b40203a5d726
Kim Fung <fungtszto@yahoo.com>
2020-01-22 14:16:27 +0000

    Create graphics.jl

---
+++ hangman/graphics.jl
@@ -0,0 +1,57 @@
+hangman1 = """
+                 _____
+                |     |
+                      |
+                      |
+                      |
+                     _|_"""
+
+hangman2 = """
+                 _____
+                |     |
+                O     |
+                      |
+                      |
+                     _|_"""
+
+hangman3 = """
+                 _____
+                |     |
+                O     |
+                |     |
+                |     |
+                     _|_"""
+
+hangman4 = """
+                 _____
+                |     |
+                O     |
+               /|     |
+                |     |
+                     _|_"""
+
+hangman5 = """
+                 _____
+                |     |
+                O     |
+               /|\\    |
+                |     |
+                     _|_"""
+
+hangman6 = """
+                 _____
+                |     |
+                O     |
+               /|\\    |
+                |     |
+               /     _|_"""
+
+hangman7 = """
+                 _____
+                |     |
+                O     |
+               /|\\    |
+                |     |
+               / \\   _|_"""
+
+hangman = [hangman1, hangman2, hangman3, hangman4, hangman5, hangman6, hangman7]
